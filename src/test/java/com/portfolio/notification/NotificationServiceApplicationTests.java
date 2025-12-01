22eb60912b1e706471e146b815db83454ac98857
roralo <roberto.rajo.lopez@gmail.com>
2025-12-01 15:59:16 +0100

    test: provide jwt decoder for context load

--- src/test/java/com/portfolio/notification/NotificationServiceApplicationTests.java
+++ src/test/java/com/portfolio/notification/NotificationServiceApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+
+import com.portfolio.notification.config.TestJwtDecoderConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
     "spring.rabbitmq.listener.simple.autoStartup=false",
     "spring.rabbitmq.listener.direct.autoStartup=false"
 })
+@Import(TestJwtDecoderConfig.class)
 class NotificationServiceApplicationTests {
 
     @Autowired
